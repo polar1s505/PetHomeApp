f95f2d2a818e1343d637d4fbb2220e71b36ef744
Polar1s <k.nadelyaev17@gmail.com>
2024-05-12 15:27:15 +0300

    small fix

--- PetHomeApp/App.cs
+++ PetHomeApp/App.cs
@@ -486,9 +486,9 @@
         {
             Console.Clear();
 
-            Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
-            Console.WriteLine(" 1. List all of our current pet information");
-            Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+            Console.WriteLine("Welcome to the PetHome app. Your main menu options are:");
+            Console.WriteLine(" 1. List all of our current animals information");
+            Console.WriteLine(" 2. Add a new pet");
             Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
             Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
             Console.WriteLine(" 5. Edit an animal’s age");
